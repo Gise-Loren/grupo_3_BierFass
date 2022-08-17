1516982296589663491d31d277163eb5e994cc79
Lucas Alvarez <108770263+lucaas2120@users.noreply.github.com>
2022-08-17 01:08:44 -0300

    Add html y css

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views/home.html")))
+
+app.get("/ageDate",(req,res) => res.sendFile(path.join(__dirname,"views/ageDate.html")))
+
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views/prueba.html")))
+
+app.get("/catalogo",(req,res) => res.sendFile(path.join(__dirname,"views/productos.html")))
+
+app.get("/catalogo/artesanal",(req,res) => res.sendFile(path.join(__dirname,"views/artesanal.html")))
+
+app.get("/catalogo/andes-Origen",(req,res) => res.sendFile(path.join(__dirname,"views/andes-origen.html")))
+
+app.get("/catalogo/patagonia",(req,res) => res.sendFile(path.join(__dirname,"views/patagonia.html")))
+
+app.get("/catalogo/quilmes",(req,res) => res.sendFile(path.join(__dirname,"views/quilmes.html")))
+
+
+app.get("/catalogo/imperial",(req,res) => res.sendFile(path.join(__dirname,"views/imperial.html")))
+
+
+app.listen(4000,()=> console.log("Servidor corriendo en el puerto 4000"))
+
