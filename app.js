90e19981fd40ef24ef5477e156fc21223598c9f7
lucaas2120 <lucaas2120@gmail.com>
2022-08-17 19:26:58 -0300

    lucas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.static('public'));
 
+
 app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views/home.html")))
 
 app.get("/ageDate",(req,res) => res.sendFile(path.join(__dirname,"views/ageDate.html")))
@@ -23,11 +24,15 @@ app.get("/catalogo/patagonia",(req,res) => res.sendFile(path.join(__dirname,"vie
 
 app.get("/catalogo/quilmes",(req,res) => res.sendFile(path.join(__dirname,"views/quilmes.html")))
 
-
 app.get("/catalogo/imperial",(req,res) => res.sendFile(path.join(__dirname,"views/imperial.html")))
+
 app.get("/carrito",(req,res) => res.sendFile(path.join(__dirname,"views/carrito.html")))
+
 app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views/login.html")))
+
 app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views/register.html")))
+
 app.get("/clasica",(req,res) => res.sendFile(path.join(__dirname,"views/descripcion.html")));
+
 app.listen(4000,()=> console.log("Servidor corriendo en el puerto 4000"))
 
