bb95048d4a68bb4a4113d9a5213ec9c3239c512c
lucaas2120 <lucaas2120@gmail.com>
2022-09-14 21:43:11 -0300

    actualizacion

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -6,15 +6,18 @@ const usersJson = path.join(__dirname, '../data/users.json');
 
 const listOFUsers = JSON.parse(fs.readFileSync(usersJson, 'utf8'));
 
+const bcrypt = require('bcryptjs');
+
 const usersControllers = {
     register: (req, res) => res.render('register'),
     registerProcess: (req, res) => {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
         let newUser = {
             id: listOFUsers.length + 1,
             name: req.body.name,
             lastName: req.body.lastName,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
             category: "user",
         }
         if (req.files) {
@@ -24,8 +27,19 @@ const usersControllers = {
         fs.writeFileSync(usersJson, JSON.stringify(listOFUsers, null, ' '));
         res.redirect('/products');
     },
-    login: (req, res) => res.render('login'),
+    login: (req, res) => {
+        const userData = req.body;
+        const selectedUser = listOFUsers.find(user=> user.email == userData.email)
+        if(selectedUser){
+            bcrypt.compareSync(user)
+        }else {
+
+        }
+        res.render('login')
+}
 }
 
 
 module.exports = usersControllers;
+
+ 
