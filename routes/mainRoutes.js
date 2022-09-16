bfb3cc89d9670fdf002d98fb3fbd9a07482e6851
lucaas2120 <lucaas2120@gmail.com>
2022-09-15 23:21:01 -0300

    probando validacion de edad

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -16,13 +16,5 @@ router.get('/home', mainController.home);
 
 router.get('/cart', mainController.cart);
 
-/* router.get('/login', mainController.login); */
-
-/* router.get('/register', mainController.register);
-
-router.post('/register', uploadUsers.any(), mainController.registerProcess);
-
-router.get('/description', mainController.description);
- */
 
 module.exports = router;
