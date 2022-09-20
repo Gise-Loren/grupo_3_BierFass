43f81a287cb08cf6f712a847372004734aa4ecf7
lucaas2120 <lucaas2120@gmail.com>
2022-09-19 21:13:42 -0300

    Agregando validacion de edad

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -4,19 +4,35 @@ const path = require('path');
 
 const controller = {
     index: (req, res) => res.render('redirect'),
-    ageDate: (req, res) =>res.render('ageDate'),
-    processAgeDate: (req, res) => {
-        const date = req.body
-console.log(date)
-
-/* const fullAge = (fechaDeNacimiento) => {
-    const fechaActual = new Date();
-console.log(fechaActual)
-} */ res.redirect('/home');
+    ageDate: (req, res) => {
+        res.render('ageDate');
+       
+    },
+    processAgeDate:  (req, res) => {
+        const day = req.body.day;
+        const month = req.body.month;
+        const year = req.body.year;
+        const currentDate = new Date();
+        const currentYear = parseInt(currentDate.getFullYear());
+        const currentMonth = parseInt(currentDate.getMonth()) + 1;
+        const currentDay = parseInt(currentDate.getDate());
+       let date = currentYear - year ;  
+        if(currentMonth< month){
+            date = date - 1;
+        } else if (currentMonth == month) {
+            if (currentDay < day){
+                date = date - 1;
+            }
+        } 
+        if (date > 17){
+            res.redirect('/home')
+        } else {
+            res.redirect('/products')
+        }
     },
     home: (req, res) => res.render('home'),
     cart: (req, res) => res.render('carrito'),
     description: (req, res) => res.render('descripcion'),
-    formUser: (req, res) =>res.render('formUser'),
+    formUser: (req, res) => res.render('formUser'),
 }
 module.exports = controller;
