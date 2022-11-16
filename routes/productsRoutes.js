095726bbeeff7b2f171ea558d4521914d66498e4
GiancarloGuitre <giancarlosalasia@hotmail.com>
2022-11-15 21:45:24 -0300

    validaciones Sprint 7

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const { prodcutsProcess } = require("../controllers/productsControllers");
+const validationsMiddleware = require ("../middlewares/validationsMiddleware");
 
 const router = express.Router();
 
@@ -19,11 +20,11 @@ router.get('/create', productsControllers.createProducts); // <------ FORMULARIO
 
 router.get('/:id', productsControllers.productsId); // <------ DETALLE DE UN PRODUCTO EN PARTICULAR.
 
-router.post('/', upload.any(), productsControllers.prodcutsProcess); // <------ ACCION DE CREACION DE PRODUCTOS.
+router.post('/', upload.any(), validationsMiddleware.validationProduct, productsControllers.prodcutsProcess); // <------ ACCION DE CREACION DE PRODUCTOS.
 
 
 
-router.put("/:id", upload.any() ,productsControllers.updateProducts); // <------ ACCION DE EDICION DE PRODUCTOS.
+router.put("/:id", upload.any(), validationsMiddleware.validationProduct, productsControllers.updateProducts); // <------ ACCION DE EDICION DE PRODUCTOS.
  
 router.delete("/:id", productsControllers.deleteProducts); // <------ ACCION DE BORRADO.
 
